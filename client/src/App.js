01ad4f165213aba993a7d98cf407791fb0d6c90e
VidathAmarasekara <vidathamarasekara99@gmail.com>
2023-07-09 16:48:20 +0530

    change some routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,9 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 
+import Login from './components/login_component'
+import SignUp from './components/signup_component'
 
-import FuelStationSignUp from './components/FuelStationSignUp'
-import FuelDetailForm from './components/FuelDetailForm'
 function App() {
   return (
     <Router>
@@ -24,19 +24,9 @@ function App() {
                 </li>
                 <li className="nav-item">
                   <Link className="nav-link" to={'/sign-up'}>
-                    Fuel Station Sign Up
+                    Sign Up
                   </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign'}>
-                   Fuel
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to={'/sign-up'}>
-                    User Sign Up
-                  </Link>
-                </li>
+                </li>                
               </ul>
             </div>
           </div>
@@ -45,9 +35,8 @@ function App() {
         <div className="auth-wrapper">
           <div className="auth-inner">
             <Routes>
-            
-              <Route path="/sign-up" element={<FuelStationSignUp />} />
-              <Route path="/sign" element={<FuelDetailForm />} />
+              <Route path="/sign-in" element={<Login />} />
+              <Route path="/sign-up" element={<SignUp />} />              
             </Routes>
           </div>
         </div>
